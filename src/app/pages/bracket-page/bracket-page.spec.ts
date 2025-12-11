937a632c612c73e661a54c9966e3e47349d9ab06
cplanchet <masterblasterplanchet@gmail.com>
2025-12-11 13:33:57 -0500

    Update tests

--- src/app/pages/bracket-page/bracket-page.spec.ts
+++ src/app/pages/bracket-page/bracket-page.spec.ts
@@ -3,10 +3,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BracketPage } from './bracket-page';
 import { BracketService } from '../../services/bracket-service';
 import { BehaviorSubject } from 'rxjs';
+import { NavigationService, Page } from '../../services/navigation-service';
+import { Bracket } from '../../models/bracket';
 
 describe('BracketPage', () => {
   const bracketServiceWinner = new BehaviorSubject<string | null>(null);
-  const bracketServiceTeams = new BehaviorSubject<string | null>(null);
+  const bracketServiceTeams = new BehaviorSubject<Bracket | null>(null);
   const mockBracketService = {
     winner$: bracketServiceWinner.asObservable(),
     teams$: bracketServiceTeams.asObservable(),
@@ -14,12 +16,16 @@ describe('BracketPage', () => {
 
   let component: BracketPage;
   let fixture: ComponentFixture<BracketPage>;
+  let navigateToSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BracketPage],
       providers: [{ provide: BracketService, useValue: mockBracketService }],
     }).compileComponents();
+
+    const navigationService = TestBed.inject(NavigationService);
+    navigateToSpy = spyOn(navigationService, 'navigateTo');
 
     fixture = TestBed.createComponent(BracketPage);
     component = fixture.componentInstance;
@@ -29,5 +35,12 @@ describe('BracketPage', () => {
   it('should create', () => {
     //TODO: finish tests
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate when bracket service emits a winner', () => {
+    bracketServiceWinner.next('winner');
+    bracketServiceTeams.next(new Bracket(['team1', 'team2']));
+
+    expect(navigateToSpy).toHaveBeenCalledWith(Page.HOME);
   });
 });
